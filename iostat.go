21e14f1046a4f3dabd6e8b965ac317c2a63311eb
Ben Aldrich <vrecan@gmail.com>
2014-06-23 21:41:42 -0600

    yay disk reading

--- iostat.go
+++ iostat.go
@@ -2,8 +2,24 @@ package main
 
 import (
    "fmt"
+   "os"
+   "log"
 )
 
+const linuxDiskStats = "/proc/diskstats"
+
 func main() {
-  fmt.Println("hello World");
+  
+	file, err := os.Open(linuxDiskStats)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+	log.Fatal(err)
+}
+fmt.Printf("read %d bytes: %q\n", count, data[:count])
+
 }
