3f188b6b5c1a373187a1e74c68150384ecfb24c0
Ben Aldrich <vrecan@gmail.com>
2014-06-27 10:51:05 -0600

    fully working iostat with read write stats for merges and completes

--- ioStatTransform/ioStatTransform.go
+++ ioStatTransform/ioStatTransform.go
@@ -3,8 +3,10 @@ import(
    "fmt"
     "../diskStat"
     "errors"
+    "regexp"
    )
 var LastRawStat = make(map[string]diskStat.DiskStat)	
+var partition = regexp.MustCompile(`\w.*\d`)
 
 func TransformStat(channel <-chan diskStat.DiskStat) (err error) {
 for {
@@ -13,21 +15,28 @@ for {
 
 		if in {
 			//ignore partitions with no history of activity
-			if(stat.ReadsCompleted == 0 && stat.WritesCompleted == 0) {
+			if((stat.ReadsCompleted == 0 && stat.WritesCompleted == 0) || partition.MatchString(stat.Device)) {
 				continue
 			}
 			timeDiffMilli,err := getTimeDiffMilli(prevStat.RecordTime, stat.RecordTime)
 			if(nil != err) { fmt.Println(err);continue}
 			readsMerged,err := getOneSecondAvg(prevStat.ReadsMerged, stat.ReadsMerged, timeDiffMilli)
+			if(nil != err) { fmt.Println(err);continue}
+			writesMerged,err := getOneSecondAvg(prevStat.WritesMerged, stat.WritesMerged, timeDiffMilli)
+			if(nil != err) { fmt.Println(err);continue}
+			writes,err := getOneSecondAvg(prevStat.WritesCompleted, stat.WritesCompleted, timeDiffMilli)
 			if(nil != err) { fmt.Println(err);continue}			
+
+			// sectorsRead,err := getOneSecondAvg(prevStat.SectorsRead, stat.SectorsRead, timeDiffMilli)
+			// if(nil != err) { fmt.Println(err);continue}						
 			reads,err := getOneSecondAvg(prevStat.ReadsCompleted, stat.ReadsCompleted, timeDiffMilli)
 			if(nil != err) { fmt.Println(err);continue}
-			fmt.Printf( "%s:  rrqm/s %.2f r/s %.2f  timeDiff %f \n\n", stat.Device, readsMerged, reads, timeDiffMilli)
-
-		} 
+			fmt.Printf( "%s:  rrqm/s %.2f wrqm/s %.2f r/s %.2f w/s %.2f \n\n", 
+				stat.Device, readsMerged, writesMerged, reads, writes)
+		}
 		LastRawStat[stat.Device] = stat
 	}
-} 
+}
 
 func getTimeDiffMilli( old int64,  cur int64) (r float64, err error){
 	if(old >= cur) {
@@ -43,8 +52,6 @@ func getOneSecondAvg(old int64, cur int64, time float64) (r float64, err error) 
 		err= errors.New("A stat has rolled over!")
 		return
 	}
-	fmt.Println(cur);
-	fmt.Println(old);
 	r = float64(float64(cur - old) / time) * 1000
 	return
 }
