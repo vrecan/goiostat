c77106a8b4d81f41f6ec38c4454ae970842de400
Ben Aldrich <vrecan@gmail.com>
2014-07-05 21:32:19 -0600

    better

--- bin/run_tests_travis.sh
+++ bin/run_tests_travis.sh
@@ -1,7 +1,7 @@
-export GOPATH:/home/travis/gopath/src/github.com/CapillarySoftware/goiostat
-export PATH=$PATH:$GOPATH/bin 
-export GOBIN=$GOPATH/bin
-go get github.com/dustin/go-humanize
-go get github.com/onsi/gomega
-go get github.com/onsi/ginkgo
+#export GOPATH:/home/travis/gopath/src/github.com/CapillarySoftware/goiostat
+#export PATH=$PATH:$GOPATH/bin 
+#export GOBIN=$GOPATH/bin
+#go get github.com/dustin/go-humanize
+#go get github.com/onsi/gomega
+#go get github.com/onsi/ginkgo
 ginkgo -cover -r 
