3f1c5abbec17540dd94b888317f34d056fffe5c8
Ben Aldrich <vrecan@gmail.com>
2014-07-07 16:34:32 -0600

    wired up

--- src/github.com/CapillarySoftware/goiostat/zmqOutput/zmqOutput.go
+++ src/github.com/CapillarySoftware/goiostat/zmqOutput/zmqOutput.go
@@ -19,6 +19,15 @@ func (z ZmqOutput) Connect(url string) {
 	z.sendSocket.Connect(url)
 }
 
+func (z ZmqOutput) send(data *[]byte) (r int, err error) {
+	if nil == z.sendSocket {
+		err = errors.New("Nil Socket, can't send")
+		return
+	}
+	r, err = z.sendSocket.SendBytes(*data, 0)
+	return
+}
+
 func (z ZmqOutput) Close() {
 	if nil != z.sendSocket {
 		z.sendSocket.Close()
@@ -41,35 +50,10 @@ func (z ZmqOutput) SendStats(eStat *ExtendedIoStats) (err error) {
 	for _, stat := range stats {
 		data, err := proto.Marshal(&stat)
 		if nil != err {
-			fmt.Println("Failed to marshal stat message : ", stat)
+			errors.New("Failed to marshal stat message : ")
 		}
 		//just print the encoded data for now... soon this will actually send a queue
-		fmt.Println(data)
+		z.send(&data)
 	}
 	return
 }
-
-// //  Socket to receive messages on
-// receiver, _ := zmq.NewSocket(zmq.PULL)
-// defer receiver.Close()
-// receiver.Connect("tcp://localhost:5557")
-
-// //  Socket to send messages to
-// sender, _ := zmq.NewSocket(zmq.PUSH)
-// defer sender.Close()
-// sender.Connect("tcp://localhost:5558")
-
-// //  Process tasks forever
-// for {
-// 	s, _ := receiver.Recv(0)
-
-// 	//  Simple progress indicator for the viewer
-// 	fmt.Print(s + ".")
-
-// 	//  Do the work
-// 	msec, _ := strconv.Atoi(s)
-// 	time.Sleep(time.Duration(msec) * time.Millisecond)
-
-// 	//  Send results to sink
-// 	sender.Send("", 0)
-// }
