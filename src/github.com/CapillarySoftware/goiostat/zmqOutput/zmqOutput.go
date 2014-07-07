ab82e5c64c7c91afe71b34f3f7f6e3a15d207b49
Ben Aldrich <vrecan@gmail.com>
2014-07-07 12:01:52 -0600

    add marshalling to have a fully functional example.. now to do queuing

--- src/github.com/CapillarySoftware/goiostat/zmqOutput/zmqOutput.go
+++ src/github.com/CapillarySoftware/goiostat/zmqOutput/zmqOutput.go
@@ -4,6 +4,7 @@ import(
 	"fmt"
 	. "github.com/CapillarySoftware/goiostat/diskStat" 
 	. "github.com/CapillarySoftware/goiostat/protoStat"
+	"code.google.com/p/goprotobuf/proto"
 
 )
 type ZmqOutput struct {
@@ -19,5 +20,12 @@ func (l ZmqOutput) SendStats (eStat *ExtendedIoStats) {
 		if(nil != err) {
 			fmt.Println(err)
 		}
-	    fmt.Println(stats)
+		for _, stat := range stats {
+			data, err := proto.Marshal(&stat)
+			if(nil != err) {
+				fmt.Println("Failed to marshal stat message : ", stat)
+			}
+			//just print the encoded data for now... soon this will actually send a queue
+			fmt.Println(data)
+		}
 }
