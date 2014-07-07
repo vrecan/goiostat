dc0355e0a13e5011ae5505d584392d435e94f5ef
Ben Aldrich <vrecan@gmail.com>
2014-07-07 12:33:55 -0600

    Update install_dependencies.sh

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -1,4 +1,5 @@
 go get github.com/dustin/go-humanize
 go get github.com/onsi/gomega
 go get github.com/onsi/ginkgo
+go get code.google.com/p/go.tools/cmd/cover
 go get code.google.com/p/goprotobuf/proto
