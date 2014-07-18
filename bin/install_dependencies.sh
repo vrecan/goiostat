d4ef1b0380a7acc916e8f97385e9565d657cc2d2
Ben Aldrich <vrecan@gmail.com>
2014-07-17 22:04:36 -0600

    reorg

--- bin/install_dependencies.sh
+++
@@ -1,6 +0,0 @@
-go get github.com/dustin/go-humanize
-go get github.com/onsi/gomega
-go get github.com/onsi/ginkgo
-go get code.google.com/p/go.tools/cmd/cover
-go get code.google.com/p/goprotobuf/proto
-go get github.com/pebbe/zmq3
